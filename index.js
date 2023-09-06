59e8fc687cb2ffedbf91991086041a0e11209228
Khalidprithy <khalidprithy@gmail.com>
2023-09-06 23:51:37 +0600

    home page added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const { verifyApiKeyInHeader, getUserIp } = require('./middleware/userAuth');
 const fixturesRoute = require('./routes/fixtures');
 const clearCacheRoute = require('./routes/clear-cache');
+const path = require('path');
 
 dotenv.config();
 
@@ -20,8 +21,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(verifyApiKeyInHeader(API_KEY));
-app.use(getUserIp);
+app.use(getUserIp); // Apply getUserIp middleware globally
+app.use('/api/admin/v1', verifyApiKeyInHeader(API_KEY)); // Apply api_key middleware to specific routes
+
+// Serve static files from the root directory (including index.html)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/admin/v1', fixturesRoute);
